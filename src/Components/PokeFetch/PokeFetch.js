67e54835719d609e1db455e253a5d62a8f176c09
kkordesh <kkordesh@iu.edu>
2022-02-25 16:27:40 -0500

    commit

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -15,6 +15,7 @@ class PokeFetch extends Component {
     let min = Math.ceil(1);
     let max = Math.floor(152);
     let pokeNum = Math.floor(Math.random() * (max - min) + min);
+    this.setState({remainingTime: 10})
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`, {
       method: "GET",
     })
@@ -24,10 +25,36 @@ class PokeFetch extends Component {
           pokeInfo: res,
           pokeSprite: res.sprites.front_default,
           pokeName: res.species.name,
+          remainingTime: 10
         });
       })
       .catch((err) => console.log(err));
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.pokeName !== this.state.pokeName) {
+      this.updateTimer()
+    }
+  }
+
+
+  updateTimer = () => {
+    let internalTimeRemaining = 10 
+
+    const countdown = () => {
+      internalTimeRemaining = internalTimeRemaining - 1 
+
+      if (internalTimeRemaining<= 0) {
+        clearInterval(newTimer);
+        this.setState({remainingTime: 0})
+        return
+      }
+
+      this.setState({remainingTime: internalTimeRemaining})
+    };
+
+    let newTimer = setInterval(countdown, 1000)
+  };
 
   render() {
     return (
@@ -35,14 +62,15 @@ class PokeFetch extends Component {
         <button className={"start"} onClick={() => this.fetchPokemon()}>
           Start!
         </button>
-        <h1 className={"timer"}>Timer Display</h1>
+        <h1 className={"timer"}>Timer Display: {this.state.remainingTime}</h1>
         <div className={"pokeWrap"}>
           <img
+          style={this.state.remainingTime === 0 ? {filter: "brightness(100%)"} : {filter: "brightness(0%)"}}
             className={"pokeImg"}
             src={this.state.pokeSprite}
             alt="pokemon"
           />
-          <h1 className={"pokeName"}>{this.state.pokeName}</h1>
+          {this.state.remainingTime === 0 ?  <h1 className={"pokeName"}>{this.state.pokeName}</h1> : null}
         </div>
       </div>
     );
